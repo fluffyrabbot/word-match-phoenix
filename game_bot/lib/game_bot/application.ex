921bcb00cc772eba67fc4fbed5d9e45dce65bc89
fluffy rabbit <ktw3000@gmail.com>
2025-03-03 23:52:57 -0800

    Add .gitattributes for line ending normalization

--- game_bot/lib/game_bot/application.ex
+++ game_bot/lib/game_bot/application.ex
@@ -1,1 +1,23 @@
-﻿defmodule GameBot.Application do  # See https://hexdocs.pm/elixir/Application.html  # for more information on OTP Applications  @moduledoc false  use Application  @impl true  def start(_type, _args) do    children = [      # Start the WordService      GameBot.Domain.WordService,            # Add other supervisors and workers here      # ...    ]    # See https://hexdocs.pm/elixir/Supervisor.html    # for other strategies and supported options    opts = [strategy: :one_for_one, name: GameBot.Supervisor]    Supervisor.start_link(children, opts)  endend 
+﻿defmodule GameBot.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  @impl true
+  def start(_type, _args) do
+    children = [
+      # Start the WordService
+      GameBot.Domain.WordService,
+
+      # Add other supervisors and workers here
+      # ...
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: GameBot.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
